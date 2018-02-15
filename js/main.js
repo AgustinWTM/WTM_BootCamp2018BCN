401d1709de1a2cdf09a9e591bf7272ca1fabca00
alejambo <30831176+alejambo@users.noreply.github.com>
2018-02-15 14:48:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-var main_bootcamps = {
-
-  init: function(){
-    console.log(data);
-    return 0;
-  }
-};
-function mainPageLoader(){
-  main_bootcamps.init();
+function sendData(){
+ infoTransferenceController();
 }
+
+sendData();
