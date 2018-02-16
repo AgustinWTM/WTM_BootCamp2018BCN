719d7514fa091a3d60781b04736feea9348a7b93
Marina Camacho <marinacamachovarea@outlook.com>
2018-02-16 14:11:31 +0100

    Actualización

--- templates/hugePicManager.js
+++ templates/hugePicManager.js
@@ -1,32 +1,38 @@
-var HugePicManager = function (){
-    this.img;
-    this.name;
-    this.description;
-    this.reference;
-    this.initPic = function(img,name,description,reference){
-        this.img = img
-        this.name = name
-        this.description = description
-        this.reference = reference
-    }
+var HugePicManager = function (img,name,description,reference){
+    this.img = img;
+    this.name = name;
+    this.description = description;
+    this.reference = reference;
+    
     this.showPic = function(){
-       document.getElementById("section2").innerHTML=  "<img src = " + this.img + " >"
+       var img = document.createElement("img");
+       img.setAttribute("src", this.img); 
+       document.getElementById("section2").appendChild(img);
+
        var name = document.createElement("h3");
        name.innerHTML = this.name;
        document.getElementById("section2").appendChild(name);
+
        var desc = document.createElement("p");
        desc.innerHTML = this.description;
        document.getElementById("section2").appendChild(desc);
+
        var ref = document.createElement("p");
        ref.innerHTML = this.reference;
        document.getElementById("section2").appendChild(ref);
     }
 
     return this;
-}
+};
+
+
+
+
+
+
 function prueba(){
-  var hugePic = new HugePicManager()
-  hugePic.initPic("resources/imgA.jpg","foto1","hola descripcion","www.google.com")
+  var hugePic = new HugePicManager("resources/imgA.jpg","foto1","hola descripcion","www.google.com")
+  
   console.log(hugePic);
   hugePic.showPic();
   return 0;
