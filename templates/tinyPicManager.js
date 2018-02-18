2fddc7e966ac4607e604414b1142e9f8378fc155
dacear <35890041+dacear@users.noreply.github.com>
2018-02-18 18:56:13 +0100

    Update tinyPicManager.js

--- templates/tinyPicManager.js
+++ templates/tinyPicManager.js
@@ -1,27 +1,52 @@
-var imagenes = function tinyPicManager() {
+var tinyPicManager = function (img,name){
+    
+      this.listaCollection = [];
+      this.img = img;
+      this.name = name;
 
-      this.foto = [{
-                'img': "resources/imgA.jpg",
-                'classUse': 'classTipeA',
-                'name':'Imagen 1'
-              },
-          {
-                        'img': "resources/imgB.jpg",
-                        'classUse': 'classTipeA',
-                        'name':'Imagen 2'
-                      }
-                    {
-                        'img': "resources/imgB.jpg",
-                        'classUse': 'classTipeA',
-                        'name':'Imagen 3'
+      this.showPic = function(){
+         var img = document.createElement("img");
+         img.setAttribute("src", this.img); 
+         document.getElementById("section1").appendChild(img);
 
-                    }]
-    var carrusel = this.foto.length;
-    for (i=0; i<carrusel; i++){
-       parentImag = document.createElement('img')[i];
-       document.getElementById(section1).appendChild(parentImag);
-       document.getElementById(section1).innerHTML= parentImag;
+         var name = document.createElement("h2");
+         name.innerHTML = this.name;
+         document.getElementById("section1").appendChild(name);
+      };
 
-
-      }
+      return this;
     };
+    
+    this.procesate = function (coleccion){
+        var coleccionSize = coleccion.length;
+        for (var i = 0; i < coleccionSize; i++) {
+          this.listaCollection.push(tinyPicManager()(this.listaCollection.lenght, coleccion[i].img, coleccion[i],name));
+
+        }
+      return 0;
+    };
+
+    this.carrusel = function() {
+        var html = "";
+        var coleccionSize = listaCollection.lenght;
+        for(var i = 0; i<coleccionSize; i++){
+          html = html + this.section1Nodo(this.listaCollection[i]);
+
+        }
+      return html;
+
+    };
+
+    this.section1Nodo = function(currentNode){
+      return  '<section id= section1 class="section1MainClass" id="'+currentNode.blockId+'">'+
+                      '<img class="'+currentNode.name+'" src="'+currentNode.img+'">'
+                    '</section>';
+
+      return this;
+
+    };  
+
+
+
+
+
