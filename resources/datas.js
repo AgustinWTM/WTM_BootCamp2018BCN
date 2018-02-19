0bb231ef072112b3a5f78622d316dde228f44bda
AgustinWTM <agustin.rodilla.wtm@gmail.com>
2018-02-19 19:26:37 +0100

    node.js

--- resources/datas.js
+++ resources/datas.js
@@ -1,16 +1,37 @@
 exports.datasGenerator = [
     {
-      name: 'aaaaa',
-      imgSRC: 'sdgdfgsgws',
+      name: 'drink',
+      imgSRC: 'https://img.kaloo.ga/thumb?url=https%3A%2F%2Fbiotrendies.com%2Fwp-content%2Fuploads%2F2015%2F07%2Fbebidas-menos-calorias.jpg&md5val=287a1fcc7c8a5a811b341571f6520a55&key=ec6e5b89091350629ced2687b1273f2ac06ed6e1&method=fill&size=708x388',
       type: 'img',
-      origin: 'dlfbndflgdf',
+      origin: 'https://biotrendies.com/rankings/bebidas/que-bebida-tiene-menos-calorias#!kalooga-26678/~%22calcular%20calorias%22%20~%22calorias%20alcohol%22%5E0.75%20~%22calorias%20bebidas%20alcoholicas%22%5E0.56%20~%22calorias%20cubata%22%5E0.42%20~%22calorias%20coctel%22%5E0.32',
       options: {}
     },
     {
-      name: 'tttt',
-      imgSRC: 'jygykfj',
+      name: 'foot',
+      imgSRC: 'http://www.telegraph.co.uk/content/dam/business/2018/01/17/pizza_trans_NvBQzQNjv4BqZgEkZX3M936N5BQK4Va8RWtT0gK_6EfZT336f62EI5U.jpg?imwidth=1400',
       type: 'img',
-      origin: 'dlfkyufkutf',
+      origin: 'http://www.telegraph.co.uk/business/2018/01/17/chicken-king-sells-goodfellas-pizza-birds-eye-owner-nomad-foods/',
+      options: {}
+    },
+    {
+      name: 'relog',
+      imgSRC: 'https://i2.linio.com/p/921e854b67957164ace43b8f626783e9-product.jpg',
+      type: 'img',
+      origin: 'https://i2.linio.com/p/921e854b67957164ace43b8f626783e9-product.jpg',
+      options: {}
+    },
+    {
+      name: 'swatch',
+      imgSRC: 'https://www.relojeriajoyeria.com/media/catalog/product/cache/9/image/370x370/9df78eab33525d08d6e5fb8d27136e95/r/e/reloj-bolsillo-viceroy-44105-02.jpg',
+      type: 'img',
+      origin: 'https://www.relojeriajoyeria.com/reloj-de-bolsillo-viceroy-44105-02',
+      options: {}
+    },
+    {
+      name: 'ortera',
+      imgSRC: 'http://autollaves.es/wp-content/uploads/2017/03/reprogramacion-de-coches.jpg',
+      type: 'img',
+      origin: 'http://autollaves.es/reprogramar-centralita-de-coche/',
       options: {}
     }
   ];
