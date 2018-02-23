6ed30e991c6f93a8a5b748d4c598382084a03743
AdmanMedia <Adman@localhost.localdomain>
2018-02-23 20:15:17 +0100

    ok

--- templates/tinyPicManager.js
+++ templates/tinyPicManager.js
@@ -1,18 +1,18 @@
 class tinyPicManager{
   constructor(s1){
-    this.s1 = s1; 
+    this.s1 = s1;
   }
 
   showPic(node, evento){
     var img = document.createElement("img");
+         img.classList.add("classTipeA");
          img.setAttribute("src", node.imgSRC);
          img.addEventListener("click", evento )
          this.s1.appendChild(img);
-         
+
+
   }
 
 
-  
-}
 
-      
+}
