fa99e662cabfb1313b43b984ce704c5967fd2966
AgustinWTM <agustin.rodilla.wtm@gmail.com>
2018-02-25 14:07:00 +0100

    Better use of node js

--- test_node.js
+++ test_node.js
@@ -1,19 +1,33 @@
-var http = require('http');
+// var http = require('http');
 var fs = require('fs');
 var dt = require('./resources/datas');
 var dtp = require('./datas_procesor.js');
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var data = dt.datasGenerator;
+var express = require('express');
+var app = express();
 
-  if(req.url === '/coolView'){
-    fs.readFile('./templates/data_reader.html', (err, dataReaded) => {
-      let workProcesor = new dtp.dataProcessor(data);
-      res.write(workProcesor.injectDatas(dataReaded));
-      res.end();
-    });
-  }else{
-    res.end(JSON.stringify(data));
-  }
-}).listen(8080);
+var coolViewManager = (req, res) => {
+  fs.readFile('./templates/data_reader.html', (err, dataReaded) => {
+    let workProcesor = new dtp.dataProcessor(dt.datasGenerator);
+    res.send(workProcesor.injectDatas(dataReaded));
+  });
+}
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(dt.datasGenerator));
+});
+
+app.get('/coolView', coolViewManager);
+
+var server = app.listen(8081, () => {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
