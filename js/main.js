54944f3b2cee1aee2559ed9c2cac8d26b351d93c
agustinrodilla <a.rodilla.tierz@gmail.com>
2018-02-25 14:13:42 +0100

    ajax in practices

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 var main_bootcamps = {
 
     init: function(){
-     var newInfo = new infoTransferenceController(data);
-     newInfo.init();
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+         var newInfo = new infoTransferenceController(this.responseText);
+         newInfo.init();
+        }
+      };
+      // xhttp.open("GET", "http://192.168.1.4:8081/", true);
+      xhttp.open("GET", "http://localhost:8081/", true);
+      xhttp.send();
 
     }
   };
   function mainPageLoader(){
     main_bootcamps.init();
   }
-  
